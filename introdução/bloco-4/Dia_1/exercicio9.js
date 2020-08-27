52d86fee0cd05426fe16af3da746397afa869da8
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-08-27 19:12:14 -0300

    exercicio 9 feito

---
+++ introdução/bloco-4/Dia_1/exercicio9.js
@@ -0,0 +1,12 @@
+const a = 4;
+const b = 3;
+const c = 2;
+let state;
+
+if(a % 2 != 0 || b % 2 != 0 || c % 2 != 0){
+    state = true;
+    console.log(state);
+} else {
+    state = false;
+    console.log(state);
+}
