4daccd0c2c75dc0ad10634c1b9c2ea55d867cc6d
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-08-27 19:04:43 -0300

    exercicio 7 feito

---
+++ introdução/bloco-4/Dia_1/exercicio7.js
@@ -0,0 +1,16 @@
+const nota = 70;
+if (100 >= nota && nota >= 90){
+    console.log("A")
+} else if (90 > nota && nota >= 80){
+    console.log("B")
+} else if (80 > nota && nota >= 70){
+    console.log("C")
+} else if (70 > nota && nota >= 60) {
+    console.log("D")
+} else if (60 > nota && nota >= 50) {
+    console.log("E")
+} else if (50 > nota && nota >= 0) {
+    console.log("F")
+} else {
+    console.log("nota invalida")
+}
