f3b9f26f05cdadc7b9dc77c6209feeeb951fabf5
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-08-31 15:13:24 -0300

    exercicio 1 e 2 feitos, não tenho certeza se pode a virgula mas verifico depois

--- introdução/bloco-4/Dia_3/exercicio2.js
+++ introdução/bloco-4/Dia_3/exercicio2.js
@@ -3,9 +3,5 @@ let array = [];
 
 for (counter = 0; counter <= number; counter += 1){
     array.push("*");
-    /*console.log(`${array}`);*/
-}
-
-for (counter2 = 0; counter2 <= number; counter2 +=1) {
     console.log(`${array}`);
 }
