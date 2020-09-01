4d1a297cc17c199316a1f31d7b69436b05ef2a7d
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-01 16:19:56 -0300

    exercicio 5 feito

---
+++ introdução/bloco-4/Dia_4/exercicio5.js
@@ -0,0 +1,20 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim",
+  };
+ let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim",
+ }
+ let counter;
+ for (let valor in info){
+     if (info[valor] !== info2[valor]){
+    console.log(info[valor] + " e " + info2[valor]);
+     } else {
+         console.log("ambos recorrentes");
+     }
+ }
