d87b7843e4e912bc328ae287867dcb91063a4b98
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-02 13:08:42 -0300

    mudei apenas um comentário do exercicio6

--- introdução/bloco-4/Dia_4/exercicio6.js
+++ introdução/bloco-4/Dia_4/exercicio6.js
@@ -1,5 +1,5 @@
 function palindromo(str) {
-   let reversed = str.split("").reverse().join(""); //o que isso faz é basicamente dividir uma string em array de strings, inverter as posições dos elementos e depois juntar tudo em uma string novamente tldr: inverte uma string
+   let reversed = str.split("").reverse().join(""); //divide uma string em array de strings, inverter as posições dos elementos e depois juntar tudo em uma string novamente tldr: inverte uma string
     if (str === reversed) {
         return 'true';
     } else {
