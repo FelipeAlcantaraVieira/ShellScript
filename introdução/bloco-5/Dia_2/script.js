c780006ea445495f0638ad61920ebd2569df868e
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-08 15:58:28 -0300

    exercicio 2

--- introdução/bloco-5/Dia_2/script.js
+++ introdução/bloco-5/Dia_2/script.js
@@ -2,3 +2,8 @@
 let firstHeader = document.createElement('h1');
 firstHeader.innerText = "Exercício 5.2 - JavaScript DOM"
 document.body.appendChild(firstHeader);
+// exercicio 2
+let primeiraDiv = document.createElement('div');
+primeiraDiv.className = "main-content";
+document.body.appendChild(primeiraDiv);
+// exercicio 3
