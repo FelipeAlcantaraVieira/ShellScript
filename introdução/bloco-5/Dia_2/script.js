9507c46adac923c70279a0acd4151f326530a3d6
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-08 18:23:09 -0300

    exercicios 5 e 6

--- introdução/bloco-5/Dia_2/script.js
+++ introdução/bloco-5/Dia_2/script.js
@@ -3,15 +3,23 @@ const firstHeader = document.createElement('h1');
 firstHeader.innerText = "Exercício 5.2 - JavaScript DOM"
 document.body.appendChild(firstHeader);
 // exercicio 2
-const primeiraDiv = document.createElement('div');
-primeiraDiv.className = 'main-content';
-document.body.appendChild(primeiraDiv);
+const mainDiv = document.createElement('div');
+mainDiv.className = 'main-content';
+document.body.appendChild(mainDiv);
 // exercicio 3
-const segundaDiv = document.createElement('div');
-segundaDiv.className = 'center-content';
-primeiraDiv.appendChild(segundaDiv);
+const centerDiv = document.createElement('div');
+centerDiv.className = 'center-content';
+mainDiv.appendChild(centerDiv);
 // exercicio 4
 const paragraph = document.createElement('p');
 paragraph.innerHTML = 'algum texto';
-segundaDiv.appendChild(paragraph);
+centerDiv.appendChild(paragraph);
 // exercicio 5
+const leftDiv = document.createElement('div');
+leftDiv.className = "left-content";
+mainDiv.appendChild(leftDiv);
+// exercicio 6
+const rightDiv = document.createElement('div');
+rightDiv.className = "right-content";
+mainDiv.appendChild(rightDiv);
+// exercicio 7
