283c480c61fa1164c5b5d7a4bd6093f8ecd58213
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-08 19:15:01 -0300

    exercicio 8

--- introdução/bloco-5/Dia_2/script.js
+++ introdução/bloco-5/Dia_2/script.js
@@ -2,24 +2,44 @@
 const firstHeader = document.createElement('h1');
 firstHeader.innerText = "Exercício 5.2 - JavaScript DOM"
 document.body.appendChild(firstHeader);
+
 // exercicio 2
 const mainDiv = document.createElement('div');
 mainDiv.className = 'main-content';
 document.body.appendChild(mainDiv);
+
 // exercicio 3
 const centerDiv = document.createElement('div');
 centerDiv.className = 'center-content';
 mainDiv.appendChild(centerDiv);
+
 // exercicio 4
 const paragraph = document.createElement('p');
 paragraph.innerHTML = 'algum texto';
 centerDiv.appendChild(paragraph);
+
 // exercicio 5
 const leftDiv = document.createElement('div');
-leftDiv.className = "left-content";
+leftDiv.className = 'left-content';
 mainDiv.appendChild(leftDiv);
+
 // exercicio 6
 const rightDiv = document.createElement('div');
-rightDiv.className = "right-content";
+rightDiv.className = 'right-content';
 mainDiv.appendChild(rightDiv);
+
 // exercicio 7
+const image = document.createElement('img');
+image.className = 'small-image';
+image.src = "https://picsum.photos/200";
+leftDiv.appendChild(image);
+
+// exercicio 8
+const list = document.createElement('ul');
+let array = ['Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez']
+for (let listElement of array){
+    let listElementItem = document.createElement('li');
+    listElementItem.innerText = listElement;
+    list.appendChild(listElementItem);
+}
+rightDiv.appendChild(list);
